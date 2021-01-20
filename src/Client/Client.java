8ba2bee1d8a83cc86e730c3168e17cc538342044
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-01-20 22:29:42 +0100

    workiiing on it

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,8 +1,33 @@
 package Client;
 
-public class Client {
-    public Client() {
+import java.util.Scanner;
 
+public class Client {
+    Scanner scanner;
+    String username;
+    int myId;
+    
+
+    public Client() {
+        scanner = new Scanner(System.in);
+
+
+    }
+
+    private String getString(String command) {
+        if (!command.equals("-1")) {
+            System.out.print(command + " > ");
+        }
+
+        return scanner.nextLine();
+    }
+
+    private int getInt(String command) {
+        if (!command.equals("-1")) {
+            System.out.print(command + " > ");
+        }
+
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static void main(String[] args) {
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,6 +1,10 @@
 package Server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,12 +20,47 @@ public class Server {
         scanner = new Scanner(System.in);
 
         port = getInt("The Port you are hosting on");
+        try {
+            ss = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("Cant create server socket! VERY BAD");
+        }
 
         ThreadHandleMessages threadHandleMessages = new ThreadHandleMessages("threadServerHandlerOutput");
         threadHandleMessages.start();
+
+        acceptConnections();
     }
 
-    protected String getString(String command) {
+    private void acceptConnections() {
+        while (true) {
+            try {
+                Socket s = ss.accept();
+
+                // Tell the client what ID he has
+                DataOutputStream dOut = new DataOutputStream(s.getOutputStream()); // Create new output stream, linked with the client that just connected
+                dOut.writeInt(idCounter++); // increase id then write id
+                dOut.flush(); // Send off the data
+
+                // get the username from the client that just connected
+                DataInputStream dIn = new DataInputStream(s.getInputStream()); // Create new input stream
+                String clientUsername = dIn.readUTF(); // Read text
+
+                addClientToMap(idCounter, clientUsername, s);
+
+                ThreadHandleClients threadHandleClients = new ThreadHandleClients("threadServerHandleClients", s);
+                threadHandleClients.start();
+            } catch (IOException e) {
+                System.out.println("Can't accept socket connection! VERY BAD");
+            }
+        }
+    }
+
+    private void addClientToMap(int id, String username, Socket s) {
+        clientMap.put(id, new ServerClient(id, username, s));
+    }
+
+    private String getString(String command) {
         if (!command.equals("-1")) {
             System.out.print(command + " > ");
         }
@@ -29,7 +68,7 @@ public class Server {
         return scanner.nextLine();
     }
 
-    protected int getInt(String command) {
+    private int getInt(String command) {
         if (!command.equals("-1")) {
             System.out.print(command + " > ");
         }
