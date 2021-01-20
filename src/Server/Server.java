b52a3c5257ea6efe0e49f15c3ac3b60e22293938
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-01-20 21:44:58 +0100

    Update Server.java

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,8 +1,31 @@
 package Server;
 
+import java.util.Scanner;
+
 public class Server {
+    Scanner scanner;
+    int port;
+
     public Server() {
-        
+        scanner = new Scanner(System.in);
+
+        port = getInt("On")
+    }
+
+    protected String getString(String command) {
+        if (!command.equals("-1")) {
+            System.out.print(command + stringAfterCommand);
+        }
+
+        return scanner.nextLine();
+    }
+
+    protected int getInt(String command) {
+        if (!command.equals("-1")) {
+            System.out.print(command + stringAfterCommand);
+        }
+
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static void main(String[] args) {
