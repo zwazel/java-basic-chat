bab1136e89c9760dc5a2643ba06d229d119083d6
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-01-23 23:38:05 +0100

    adding comments and reworked the sending message stuff
    its a bit cleaner now

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -65,9 +65,9 @@ public class Server {
     }
 
     private void acceptConnections() {
-        while (running) {
+        while (running) { // Only while the server is running (this is to avoid errors as I haven't found another workaround)
             try {
-                Socket s = ss.accept();
+                Socket s = ss.accept(); // Wait for a request from a client
 
                 // Tell the client what ID he has
                 DataOutputStream dOut = new DataOutputStream(s.getOutputStream()); // Create new output stream, linked with the client that just connected
@@ -85,67 +85,63 @@ public class Server {
                 ThreadHandleClient threadHandleClient = new ThreadHandleClient("threadServerHandleClients", this, s);
                 threadHandleClient.start();
 
-                idCounter++;
-            } catch (IOException e) {
+                idCounter++; // Increase the ID counter, to make sure that nobody gets the same ID
+            } catch (IOException e) { // catch error
                 System.out.println("Can't accept socket connection! VERY BAD");
             }
         }
     }
 
-    public void sendMessageToClients(String message) {
-        if(running) {
-            System.out.println(username + " (Me): " + message);
+    // Send a message from me, the server, to all the other clients.
+    public void sendMessageToClients(String message) { // We need the message we want to send
+        if(running) { // Only while the server is running (this is to avoid errors as I haven't found another workaround)
+            System.out.println(username + " (Me): " + message); // Print the message for myself
 
-            for (int i : clientMap.keySet()) {
-                Socket s = clientMap.get(i).getSocket();
+            for (int i : clientMap.keySet()) { // go through all the currently connected clients
+                Socket s = clientMap.get(i).getSocket(); // Get the socket of the current Client
+                message = username + ": " + message; // set the message so it's displaying correctly for the clients
+                sendMessage(message,s); // Send the message to the specified socket
+            }
+        }
+    }
 
-                try {
-                    DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
-                    dOut.writeByte(1);
-                    dOut.writeUTF(username + ": " + message);
-                    dOut.flush(); // Send off the data
-                } catch (IOException e) {
-                    // TODO: Tell on what client we failed
-                    System.out.println("Can't send message from Server to clients! VERY BAD");
+    // Send a message from a client to all the other clients. Don't send the message to the client himself
+    public void sendMessageFromClientToClients(int id, String message) { // We need the ID because we need to know who sent the message
+        //String username = clientsHashMap.get(id).getUsername(); // We already have the username inside of the message, so this is not needed
+
+        if(running) { // Only while the server is running (this is to avoid errors as I haven't found another workaround)
+            System.out.println(message); // print the message for myself
+            for (int i : clientMap.keySet()) { // go through all the currently connected clients
+                if (i != id) { // Don't send message to the client who sent the message
+                    Socket s = clientMap.get(i).getSocket(); // Get the socket from the current client from the hashmap, so we know where to send the message to
+                    sendMessage(message, s); // Send the message to the socket
                 }
             }
         }
     }
 
-    public void sendMessageFromClientToClients(int id, String message) {
-        //String username = clientsHashMap.get(id).getUsername(); // We already have the username inside of the message, so this is not needed
-
-        if(running) {
-            System.out.println(message);
-            for (int i : clientMap.keySet()) {
-                if (i != id) { // Don't send message to myself
-                    Socket s = clientMap.get(i).getSocket();
-
-                    try {
-                        DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
-                        dOut.writeByte(1);
-                        dOut.writeUTF(message);
-                        dOut.flush(); // Send off the data
-                    } catch (IOException e) {
-                        // TODO: Tell on what client we failed
-                        System.out.println("Can't send message from Client to other Clients! VERY BAD");
-                    }
-                }
-            }
+    // Here we send a normal message to a specified socket
+    private void sendMessage(String message, Socket s) { // Get the message and the socket
+        try {
+            DataOutputStream dOut = new DataOutputStream(s.getOutputStream()); // create new dataOutputStream where we'll put our message in
+            dOut.writeByte(1); // tell the client what type of message he's receiving (1 = default message) by writing a byte in the stream
+            dOut.writeUTF(message); // Put the message in the stream
+            dOut.flush(); // Send off the data
+        } catch (IOException e) { // catch error
+            // TODO: Tell on what client we failed
+            System.out.println("Can't send message from Server to clients! VERY BAD");
         }
     }
 
     private String getPublicIp() {
-        //Instantiating the URL class
-        URL url = null;
         String result = null;
         try {
-            url = new URL("http://ipv4bot.whatismyipaddress.com/");
+            // Set the url from where we'll be getting the data from
+            URL url = new URL("http://ipv4bot.whatismyipaddress.com/"); // Nice and fast website for getting your ipv4
 
             //Retrieving the contents of the specified page
-            Scanner sc = null;
             try {
-                sc = new Scanner(url.openStream());
+                Scanner sc = new Scanner(url.openStream());
 
                 //Instantiating the StringBuffer class to hold the result
                 StringBuffer sb = new StringBuffer();
@@ -155,6 +151,7 @@ public class Server {
                 //Retrieving the String from the String Buffer object
                 result = sb.toString();
                 result = result.replaceAll("<[^>]*>", ""); // Remove the html tags with some fancy black magic
+            // Catch error if we can't create an open stream to the provided url
             } catch (IOException e) {
                 System.out.println("Can't create openStream Scanner! VERY BAD");
             }
@@ -163,14 +160,16 @@ public class Server {
             System.out.println("Can't connect to URL! VERY BAD");
         }
 
-        // Send 
+        // return the ip, or null if we can't find an IP
         return result;
     }
 
-    private void addClientToMap(int id, String username, Socket s) {
-        clientMap.put(id, new ServerClient(id, username, s));
+    // Add a new client to the HashMap
+    private void addClientToMap(int id, String username, Socket s) { // Get the id, username and the socket
+        clientMap.put(id, new ServerClient(id, username, s)); // Create a new ServerClient Instance and safe it in the HashMap
     }
 
+    // Method for getting an Integer
     private int getInt(String command) {
         if (!command.equals("-1")) {
             System.out.print(command + " > ");
