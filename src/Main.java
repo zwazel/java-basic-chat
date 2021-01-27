898a42d7461fc40a3a83985955ab9a0db1f0e8fd
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-01-27 18:10:28 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,18 +18,15 @@ public class Main {
         System.out.print("Do you want to host (0) or to join (1)? > "); // Tell the user what to do
 
         switch (Integer.parseInt(scanner.nextLine())) { // get the input as string, and convert it to an Integer
-            case 0 -> { // case 0, behave as a server
+            case 0: // case 0, behave as a server
                 new Server(); // instantiate new server
                 return true; // our input is valid
-            }
-            case 1 -> { // case 1, behave as a client
+            case 1: // case 1, behave as a client
                 new Client(); // instantiate new client
                 return true; // our input is valid
-            }
-            default -> { // default, input is not valid
+            default: // default, input is not valid
                 System.out.println("Invalid Input! Try again"); // tell the user whats wrong
                 return false; // input is not valid
-            }
         }
     }
 }
