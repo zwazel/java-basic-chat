c61920bb4e62d1498faa86575ed0bf3731930ac3
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-01-31 01:01:38 +0100

    Create KickClientCommandHandler.java

---
+++ src/ChatCommands/KickClientCommandHandler.java
@@ -0,0 +1,18 @@
+package ChatCommands;
+
+public class KickClientCommandHandler extends AbstractCommand {
+    @Override
+    public void clientExecute(boolean isOp, String[] args, int senderId) {
+
+    }
+
+    @Override
+    public void serverExecute(String[] args) {
+
+    }
+
+    @Override
+    public void printDescription() {
+
+    }
+}
