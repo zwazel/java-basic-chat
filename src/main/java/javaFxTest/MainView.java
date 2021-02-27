137cc25f8c6e2370f28e7794efe84f60f626dd17
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-02-27 12:13:14 +0100

    Update MainView.java

--- src/main/java/javaFxTest/MainView.java
+++ src/main/java/javaFxTest/MainView.java
@@ -12,11 +12,22 @@ public class MainView extends Application {
         launch(args);
     }
 
+    /**
+     * The Stage is the whole window
+     * A scene is just a part/the content inside of the window
+     */
     @Override
     public void start(Stage primaryStage) {
+
+        /**
+         * Sets the title for the window, aka stage
+         */
         primaryStage.setTitle("Test");
 
         button = new Button("Click me"); // Instanciate new button and set the text
-        //button.setText("Click me");
+
+        /**
+         *
+         */
     }
 }
