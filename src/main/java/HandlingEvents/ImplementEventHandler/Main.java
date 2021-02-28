ee38ee1878d625cc40f34beaa8558cda781f035a
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-02-28 18:09:55 +0100

    better project structure

--- src/main/java/HandlingEvents/ImplementEventHandler/Main.java
+++ src/main/java/HandlingEvents/ImplementEventHandler/Main.java
@@ -1,4 +1,4 @@
-package HandlingEvents.HandleUserEvents;
+package HandlingEvents.ImplementEventHandler;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/HandlingEvents/ImplementEventHandler/MainView.java
+++ src/main/java/HandlingEvents/ImplementEventHandler/MainView.java
@@ -1,4 +1,4 @@
-package HandlingEvents.HandleUserEvents;
+package HandlingEvents.ImplementEventHandler;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
