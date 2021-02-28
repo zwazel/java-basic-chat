71129cf16e49680a8c44fc40974f54ff8c923f19
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2021-02-28 13:37:38 +0100

    Alert Boxes!

---
+++ src/main/java/AlertBoxes/Main.java
@@ -0,0 +1,7 @@
+package AlertBoxes;
+
+public class Main {
+    public static void main(String[] args) {
+        MainView.main(args);
+    }
+}
---
+++ src/main/java/AlertBoxes/MainView.java
@@ -0,0 +1,37 @@
+package AlertBoxes;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+    Stage window;
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * The Stage is the whole window
+     * A scene is just a part/the content inside of the window
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+
+        button = new Button("Click me");
+        button.setOnAction(e -> AlertBox.display("Alert", "Hello!"));
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+
+        window.setScene(scene);
+        window.setTitle("Alert Boxes!");
+        window.show();
+    }
+}
